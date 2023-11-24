ab664c08b5ffcc59f28c490159971cb4fef735cb
Jan Egil Staff Øvretveit <janstovr@gmail.com>
2023-11-24 07:14:22 +0100

    Add setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      Hello world
+    <div className="categories-container">
+      <div className="category-container">
+
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop now</p>
+        </div>
+
+        <div className="category-body-container">
+          <h2>Jackets</h2>
+          <p>Shop now</p>
+        </div>
+
+        <div className="category-body-container">
+          <h2>Sneakers</h2>
+          <p>Shop now</p>
+        </div>
+
+        <div className="category-body-container">
+          <h2>Womens</h2>
+          <p>Shop now</p>
+        </div>
+
+        <div className="category-body-container">
+          <h2>Men</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
     </div>
   );
 }
